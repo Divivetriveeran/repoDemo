2b7a8891a34f02be1998c11cecd75b43fb8b011d
Divya Vetriveeran <vdivya.3793@gmail.com>
2025-10-15 12:24:05 +0530

    Add Subtraction.java in branch_featureSub

---
+++ Subtraction.java
@@ -0,0 +1,7 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int sub = a - b;
+        System.out.println("Difference: " + sub);
+    }
+}
