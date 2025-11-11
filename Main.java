1d135afed45aaa08fe2c19746a4ac34ec7efabf1
Divya Vetriveeran <34702513+Divivetriveeran@users.noreply.github.com>
2025-11-11 20:44:38 +0530

    Implement main method for calculator operations

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        System.out.println("Add: " + Addition.add(a,b));
+        System.out.println("Sub: " + Subtraction.sub(a,b));
+    }
+}
