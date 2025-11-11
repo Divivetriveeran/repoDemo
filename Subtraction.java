dfae4a1a89ac3455ab99d4d55ca34553b53f7a04
Divya Vetriveeran <34702513+Divivetriveeran@users.noreply.github.com>
2025-11-11 21:13:16 +0530

    Format sub method for better readability

--- Subtraction.java
+++ Subtraction.java
@@ -1,4 +1,5 @@
-package com.example.calculator;
 public class Subtraction {
-    public static int sub(int a, int b) { return a - b; }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
 }
