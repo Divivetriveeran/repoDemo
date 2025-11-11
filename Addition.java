02693a69746c90d35f45d3760d7cf1ff09a41106
Divya Vetriveeran <34702513+Divivetriveeran@users.noreply.github.com>
2025-11-11 21:12:57 +0530

    Format add method for better readability

--- Addition.java
+++ Addition.java
@@ -1,4 +1,5 @@
-package com.example.calculator;
 public class Addition {
-    public static int add(int a, int b) { return a + b; }
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
