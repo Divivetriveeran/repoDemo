5d38314d08d1e6e6f96637954acbc1c96f62638c
Divya Vetriveeran <34702513+Divivetriveeran@users.noreply.github.com>
2025-11-11 20:46:18 +0530

    Refactor Subtraction class to remove main method
    Removed main method and print statement from Subtraction class.

--- Subtraction.java
+++ Subtraction.java
@@ -1,7 +1,4 @@
+package com.example.calculator;
 public class Subtraction {
-    public static void main(String[] args) {
-        int a = 5, b = 10;
-        int sub = a - b;
-        System.out.println("Difference: " + sub);
-    }
+    public static int sub(int a, int b) { return a - b; }
 }
