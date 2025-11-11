d27e13611a362f2d70ffee6f7a961deb3ef49d56
Divya Vetriveeran <34702513+Divivetriveeran@users.noreply.github.com>
2025-11-11 21:12:31 +0530

    Refactor main method to use direct arithmetic

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
-package com.example.calculator;
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 3;
-        System.out.println("Add: " + Addition.add(a,b));
-        System.out.println("Sub: " + Subtraction.sub(a,b));
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
     }
 }
